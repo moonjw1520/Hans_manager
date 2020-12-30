1d25f1808d1f60513ff34543c6882706b4871ad8
MoonJeongWan <moonjw1520@gmail.com>
2020-12-31 01:36:19 +0900

    first commit

---
+++ app/src/main/java/com/example/hans_manager/ActivityAdmin.kt
@@ -0,0 +1,11 @@
+package com.example.hans_manager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityAdmin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin)
+    }
+}
